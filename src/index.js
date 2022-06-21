7cfdb89bb9d1823aa35a8930c2b5a1af32e95dee
mkrause <maikel.krause@fortanix.com>
2022-06-21 14:25:08 +0200

    Implement generateZodForObjectSchema.

--- src/index.js
+++ src/index.js
@@ -5,27 +5,60 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const promises_1 = __importDefault(require("fs/promises"));
-const generateZodForSchema = (schemaId, schema) => {
+const generateZodForObjectSchema = (schemaId, schema) => {
+    const properties = schema.properties ?? {};
+    const generatedProps = [];
+    for (const [propertyName, property] of Object.entries(properties)) {
+        generatedProps.push(`${propertyName}: undefined,`);
+    }
     return `
 import * as z from 'zod';
 
-export const ${schemaId} = ...
+export const ${schemaId} = z.object({
+  ${generatedProps.join('\n')}
+});
   `.trim();
 };
+const generateZodForSchema = (schemaId, schema) => {
+    if ('items' in schema) {
+        // Case: ArraySchemaObject
+        throw new Error('Not supported');
+    }
+    else {
+        // Case: NonArraySchemaObject
+        const type = schema.type;
+        if (typeof type === 'undefined') {
+            throw new Error(`Missing 'type'`);
+        }
+        switch (type) {
+            case 'boolean': throw new Error(`Not yet supported`);
+            case 'number': throw new Error(`Not yet supported`);
+            case 'string': throw new Error(`Not yet supported`);
+            case 'integer': throw new Error(`Not yet supported`);
+            case 'object': return generateZodForObjectSchema(schemaId, schema);
+        }
+    }
+};
 const main = async () => {
-    // const parser = new SwaggerParser();
-    // const api = await SwaggerParser.validate(path.join(__dirname, '../resources/roche_api.json'));
-    // console.log(api.components.schemas.KeyObject);
     const documentContent = await promises_1.default.readFile(path_1.default.join(__dirname, '../resources/roche_api.json'), 'utf8');
     const document = JSON.parse(documentContent);
     //console.log(document.components.schemas.KeyObject);
     console.info('Converting...');
     const schemas = document.components?.schemas ?? {};
     for (const [schemaId, schema] of Object.entries(schemas)) {
-        if (!('$ref' in schema)) {
-            const generatedCode = generateZodForSchema(schemaId, schema);
-            //console.log(generatedCode); // Debug
-            await promises_1.default.writeFile(path_1.default.join(__dirname, `../generated/${schemaId}.ts`), generatedCode);
+        try {
+            if ('$ref' in schema) {
+                // Case: OpenAPIV3.ReferenceObject
+                throw new Error(`Not implemented`);
+            }
+            else {
+                // Case: OpenAPIV3.SchemaObject
+                const generatedCode = generateZodForSchema(schemaId, schema);
+                await promises_1.default.writeFile(path_1.default.join(__dirname, `../generated/${schemaId}.ts`), generatedCode);
+            }
+        }
+        catch (e) {
+            // Ignore (for now)
         }
     }
     console.info('Done!');
--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,46 @@ import path from 'path';
 import fs from 'fs/promises';
 
 import { OpenAPIV3 } from 'openapi-types';
-import SwaggerParser from '@apidevtools/swagger-parser';
 
 
-const generateZodForSchema = (schemaId: string, schema: OpenAPIV3.SchemaObject): string => {
+const generateZodForObjectSchema = (schemaId: string, schema: OpenAPIV3.NonArraySchemaObject): string => {
+  const properties = schema.properties ?? {};
+  
+  const generatedProps: Array<string> = [];
+  for (const [propertyName, property] of Object.entries(properties)) {
+    generatedProps.push(`${propertyName}: undefined,`);
+  }
+  
   return `
 import * as z from 'zod';
 
-export const ${schemaId} = ...
+export const ${schemaId} = z.object({
+  ${generatedProps.join('\n')}
+});
   `.trim();
 };
 
+const generateZodForSchema = (schemaId: string, schema: OpenAPIV3.SchemaObject): string => {
+  if ('items' in schema) {
+    // Case: ArraySchemaObject
+    throw new Error('Not supported');
+  } else {
+    // Case: NonArraySchemaObject
+    const type: undefined | OpenAPIV3.NonArraySchemaObjectType = schema.type;
+    
+    if (typeof type === 'undefined') { throw new Error(`Missing 'type'`); }
+    
+    switch (type) {
+      case 'boolean': throw new Error(`Not yet supported`);
+      case 'number': throw new Error(`Not yet supported`);
+      case 'string': throw new Error(`Not yet supported`);
+      case 'integer': throw new Error(`Not yet supported`);
+      case 'object': return generateZodForObjectSchema(schemaId, schema);
+    }
+  }
+};
+
 const main = async () => {
-  // const parser = new SwaggerParser();
-  // const api = await SwaggerParser.validate(path.join(__dirname, '../resources/roche_api.json'));
-  // console.log(api.components.schemas.KeyObject);
-  
   const documentContent = await fs.readFile(path.join(__dirname, '../resources/roche_api.json'), 'utf8');
   const document: OpenAPIV3.Document = JSON.parse(documentContent);
   //console.log(document.components.schemas.KeyObject);
@@ -27,10 +51,17 @@ const main = async () => {
   
   const schemas = document.components?.schemas ?? {};
   for (const [schemaId, schema] of Object.entries(schemas)) {
-    if (!('$ref' in schema)) {
-      const generatedCode = generateZodForSchema(schemaId, schema);
-      //console.log(generatedCode); // Debug
-      await fs.writeFile(path.join(__dirname, `../generated/${schemaId}.ts`), generatedCode);
+    try {
+      if ('$ref' in schema) {
+        // Case: OpenAPIV3.ReferenceObject
+        throw new Error(`Not implemented`);
+      } else {
+        // Case: OpenAPIV3.SchemaObject
+        const generatedCode = generateZodForSchema(schemaId, schema);
+        await fs.writeFile(path.join(__dirname, `../generated/${schemaId}.ts`), generatedCode);
+      }
+    } catch (e) {
+      // Ignore (for now)
     }
   }
   
